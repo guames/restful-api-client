1971fe4cc5e9052e0ede916d11517baa4e4de513
games <gustavo.ames87@gmail.com>
2020-02-14 20:53:48 -0300

    Initial commit

---
+++ src/main/java/com/guames/restful/api/client/restfulapiclient/RestfulApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.guames.restful.api.client.restfulapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guames/restful/api/client/restfulapiclient/RestfulApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guames.restful.api.client.restfulapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
